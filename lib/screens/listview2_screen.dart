35984e444a985b5f339a73c2c45a6b7db85b2233
Bryan Zapata <bmzc95@gmail.com>
2022-01-18 22:08:19 -0500

    more list view

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -13,11 +13,19 @@ class Listview2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Listview2Screen')),
+      appBar: AppBar(
+        title: const Text('Listview2Screen'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
       body: ListView.separated(
           itemBuilder: (context, index) => ListTile(
                 title: Text(options[index]),
-                trailing: const Icon(Icons.arrow_forward_ios),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.indigo,
+                ),
+                onTap: () {},
               ),
           separatorBuilder: (_, __) => const Divider(),
           itemCount: options.length),
