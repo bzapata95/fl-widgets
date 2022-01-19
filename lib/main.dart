a1fd0d7b46c4e84cb6e1192e39ba00ca22fb4f21
Bryan Zapata <bmzc95@gmail.com>
2022-01-18 22:02:06 -0500

    list view normal and listview separated

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:fl_components/screens/listview2_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: Listview2Screen(),
+    );
+  }
+}
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  final options = const [
+    'Megaman',
+    'Metal gEAR',
+    'Super smash',
+    'Final Fantasy'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Listview2Screen')),
+      body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+                title: Text(options[index]),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: options.length),
+    );
+  }
+}
