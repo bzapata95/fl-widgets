f3f6ac906f91efd6010f7d0e2337abcbada0653e
Bryan Zapata <bmzc95@gmail.com>
2022-01-22 16:12:21 -0500

    Form widget

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -5,31 +5,52 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(title: Text('Inputs y forms')),
       body: SingleChildScrollView(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(children: [
-          TextFormField(
-              autofocus: true,
-              initialValue: 'Bryan Zapata',
-              autocorrect: false,
-              textCapitalization: TextCapitalization.words,
-              keyboardType: TextInputType.emailAddress,
-              obscureText: true, // for password
-              onChanged: (value) => print(value),
-              validator: (value) {
-                if (value == null) return 'Este campo es requerido';
-                return value.length < 3 ? 'Mínimo de 3 letras' : null;
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(
-                  hintText: 'Nombre del usuario',
-                  labelText: 'Usuario',
-                  helperText: 'Solo letras',
-                  suffixIcon: Icon(Icons.group_add_outlined)))
-        ]),
+        child: Form(
+          key: myFormKey,
+          child: Column(children: [
+            TextFormField(
+                autofocus: true,
+                initialValue: '',
+                autocorrect: false,
+                textCapitalization: TextCapitalization.words,
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true, // for password
+                onChanged: (value) => print(value),
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Mínimo de 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                    hintText: 'Nombre del usuario',
+                    labelText: 'Usuario',
+                    helperText: 'Solo letras',
+                    suffixIcon: Icon(Icons.group_add_outlined))),
+            ElevatedButton(
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: Center(child: const Text('Guardar')),
+                ),
+                onPressed: () {
+                  //Ocultar el keyboard
+                  FocusScope.of(context).requestFocus(FocusNode());
+
+                  if (!myFormKey.currentState!.validate()) {
+                    print('Formulario no valido');
+                    return;
+                  }
+
+                  print('form values:');
+                })
+          ]),
+        ),
       )),
     );
   }
