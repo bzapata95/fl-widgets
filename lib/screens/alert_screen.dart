b41b1e59d12ff4f821c311ed17f2a6e71d888d1e
Bryan Zapata <bmzc95@gmail.com>
2022-01-21 20:51:12 -0500

    dialog cupertino

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,9 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void handleDisplayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+              title: const Text('Titulo'),
+              content: Column(mainAxisSize: MainAxisSize.min, children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100)
+              ]),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancelar')),
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Ok'))
+              ]);
+        });
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
         barrierDismissible:
             false, // "TRUE" Cierra el dialogo cuando hace click en el overlay
@@ -33,7 +59,9 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
           child: ElevatedButton(
-              onPressed: () => handleDisplayDialog(context),
+              onPressed: () => Platform.isAndroid
+                  ? displayDialogAndroid(context)
+                  : displayDialogIOS(context),
               child: const Padding(
                 padding: EdgeInsets.all(20),
                 child: Text('Mostrar alerta', style: TextStyle(fontSize: 14)),
